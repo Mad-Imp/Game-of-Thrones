4ccbf525fc9db31d5f7e90389cec1a2927433fec
MadImp <60294415+Mad-Imp@users.noreply.github.com>
2020-10-23 22:34:33 +0300

    Склонирована верстка. Подгрузка данных через API, рандомное обновление информации, подключение прелоадера

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './spinner.css';
+
+const Spinner = () => {
+    return (
+        <div className="loadingio-spinner-spin-3bmgr2fy3n2">
+            <div className="ldio-wsxtcfp3bj">
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Spinner;
+
+
